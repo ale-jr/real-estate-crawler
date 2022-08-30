21d6c620ec4e337b5cfbcd8f25c6ce4dbf76a9f0
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-08-30 11:22:27 -0300

    fix pm2 file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name   : "real-estate-crawler",
+    script : "./main.js",
+    env: {
+      "OPENSSL_CONF": "/dev/null"
+    }
+  }]
+}
--- ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-export const apps = [{
-  name: "real-estate-crawler",
-  script: "./main.js",
-  env: {
-    "OPENSSL_CONF": "/dev/null"
-  }
-}];
